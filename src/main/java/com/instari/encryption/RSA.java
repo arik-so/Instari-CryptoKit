17ed27b3e5a783515ca00b5e0468d7b3fd9d8778
Arik Sosman <arik@sosman.eu>
2015-02-23 15:44:57 +0100

    solved keypair problem for RSA

--- src/main/java/com/instari/encryption/RSA.java
+++ src/main/java/com/instari/encryption/RSA.java
@@ -4,4 +4,35 @@ package com.instari.encryption;
  * Created by arik on 2/23/15.
  */
 public class RSA {
+
+    public static RSAKeyPair generateKeyPair(){
+
+        String privateKey = null;
+        String publicKey = null;
+
+        return new RSAKeyPair(privateKey, publicKey);
+
+    }
+
+    static class RSAKeyPair{
+
+        private String privateKey;
+        private String publicKey;
+
+        private RSAKeyPair(String privateKey, String publicKey) {
+            this.privateKey = privateKey;
+            this.publicKey = publicKey;
+        }
+
+        public String getPrivateKey() {
+            return privateKey;
+        }
+
+        public String getPublicKey() {
+            return publicKey;
+        }
+    }
+
 }
+
+
