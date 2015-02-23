d10f50ab30146af7674500c395a2ae0846683f2a
Arik Sosman <arik@sosman.eu>
2015-02-23 18:02:41 +0100

    filled RSA test class with pseudo methods

--- src/test/java/com/instari/encryption/RSATest.java
+++ src/test/java/com/instari/encryption/RSATest.java
@@ -1,7 +1,43 @@
 package com.instari.encryption;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class RSATest {
 
+    @Test
+    public void testGenerateKeyPair() throws Exception {
+
+    }
+
+    @Test
+    public void testEncryptWithPublic() throws Exception {
+
+    }
+
+    @Test
+    public void testDecryptWithPrivate() throws Exception {
+
+    }
+
+    @Test
+    public void testEncryptWithPrivate() throws Exception {
+
+    }
+
+    @Test
+    public void testDecryptWithPublic() throws Exception {
+
+    }
+
+    @Test
+    public void testSign() throws Exception {
+
+    }
+
+    @Test
+    public void testIsSignatureValid() throws Exception {
+
+    }
 }
