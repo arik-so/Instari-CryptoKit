8286cb24a809e3ac2386832bbc0f3cd4a10fcfda
Arik Sosman <arik@sosman.eu>
2015-02-23 15:37:25 +0100

    key generation appears to be working

--- src/main/java/com/instari/encryption/AES.java
+++ src/main/java/com/instari/encryption/AES.java
@@ -1,7 +1,44 @@
 package com.instari.encryption;
 
+import com.instari.encoding.HexBinary;
+
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import java.security.NoSuchAlgorithmException;
+
 /**
  * Created by arik on 2/23/15.
  */
 public class AES {
+
+    public static String generateInitializationVector() throws NoSuchAlgorithmException { // 128 bits or 32 bytes
+
+        KeyGenerator keyGen = KeyGenerator.getInstance("AES");
+        keyGen.init(128); // we need 256 bits
+        SecretKey secretKey = keyGen.generateKey();
+
+        byte[] key = secretKey.getEncoded();
+        return HexBinary.binaryToHex(key);
+
+    }
+
+    public static String generateKey() throws NoSuchAlgorithmException { // 256 bits or 64 bytes
+
+        KeyGenerator keyGen = KeyGenerator.getInstance("AES");
+        keyGen.init(256); // we need 256 bits
+        SecretKey secretKey = keyGen.generateKey();
+
+        byte[] key = secretKey.getEncoded();
+        return HexBinary.binaryToHex(key);
+
+    }
+
+    public static String encrypt(String data, String key, String initializationVector){ // returns a base64-encoded string
+        return null;
+    }
+
+    public static String decrypt(String data, String key, String initializationVector){ // accepts a base64-encoded string
+        return null;
+    }
+
 }
--- src/test/java/com/instari/encryption/AESTest.java
+++ src/test/java/com/instari/encryption/AESTest.java
@@ -1,9 +1,37 @@
 package com.instari.encryption;
 
+import org.junit.Test;
+
+import java.security.NoSuchAlgorithmException;
+
 import static org.junit.Assert.*;
 
 public class AESTest {
 
-    
 
+    @Test
+    public void testGenerateInitializationVector() throws Exception {
+
+        String generatedIV = AES.generateInitializationVector();
+        assertEquals("AES IV length is 64 bytes", 32, generatedIV.length());
+
+    }
+
+    @Test
+    public void testGenerateKey() throws NoSuchAlgorithmException {
+
+        String generatedKey = AES.generateKey();
+        assertEquals("AES key length is 64 bytes", 64, generatedKey.length());
+
+    }
+
+    @Test
+    public void testEncrypt() throws Exception {
+
+    }
+
+    @Test
+    public void testDecrypt() throws Exception {
+
+    }
 }
